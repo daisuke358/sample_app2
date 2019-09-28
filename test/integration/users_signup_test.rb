4a8d7d1a083195d824837be55f07091cb081a57d
江島大輔 <eshimadaisuke@Daisuke-MacBook-Pro.local>
2019-09-29 03:50:06 +0900

    Use SSL and the Puma webserver in production

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,7 +10,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password:              "foo",
                                          password_confirmation: "bar" } }
     end
-    follow_redirect!
     assert_template 'users/new'
   end
 end
